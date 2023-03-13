3fc37278c9a41d40eadc4e0ce3a433cbf84d3f85
italo.silva <italo.silva@ssp.df.gov.br>
2023-03-13 11:18:57 -0300

    [file] done:feat - upload files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
+import { uid } from 'uid';
+import { FileService } from './file/file.service';
 
-@Controller()
+@Controller('auth')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly fileService: FileService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('doc')
+  async upload(@UploadedFile('file') file: Express.Multer.File) {
+    const path = join(__dirname, '..', 'storage', 'files', `${uid(16)}.pdf`);
+
+    return this.fileService.upload(file, path);
   }
 }
